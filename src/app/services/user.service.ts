1d62a0d8c303f5a71c88961c6769b51437ba1145
Andoni <andlopez@birt.eus>
2025-06-03 22:09:44 +0200

    Subida inicial del proyecto Angular

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,18 +8,18 @@ import { Observable } from 'rxjs';
 export class UserService {
 
   constructor(private http: HttpClient){
-  this.http.get('https://reqres.in/api/users?page=2');
+  this.http.get('https://683f53eb5b39a8039a545d27.mockapi.io/:endpoint');
 }
 
 getUsers(){
-  this.http.get('https://reqres.in/api/users?page=2').subscribe(data => {
+  this.http.get('https://683f53eb5b39a8039a545d27.mockapi.io/:endpoint').subscribe(data => {
     console.log(data);
   });
   console.log("Esto se ejecutará antes que el console log de arriba");
 }
 
 getUser(): Observable<any>{
-  return this.http.get('https://reqres.in/api/users?page=2');
+  return this.http.get('https://683f53eb5b39a8039a545d27.mockapi.io/:endpoint');
 }
 
 }
